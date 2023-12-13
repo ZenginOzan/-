a220efbfb95077df711ef15121f2a2b834c319f7
ZenginOzan <121515354+ZenginOzan@users.noreply.github.com>
2023-12-13 21:33:09 +0300

    Create sozluk.py

---
+++ sozluk.py
@@ -0,0 +1,19 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "BRUH": "Komik olaylarda söylenen cevap",
+            "SIUUU": "Portekizli futbolcu Cristiano Ronaldo'nun 'Evet!' diye bağırması",
+            "DAMN": "Garip bir olay karşısında verilen tepki",
+            "AMOGUS": "Among us.",
+            "T POSE": "T şeklindeki popüler karakter pozu ",
+            "OHIO": "ABD'de garip olaylar yaşandığı iddia edilen eyalet",
+            "YEET": "Bişeyi fırlatmak",
+            }
+            
+for i in range(3):
+    word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+    
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("VVVUUUUUUUUAAAAPPPPFFFFFŞŞŞŞŞUUUUUUAAAAAAĞĞĞ")
